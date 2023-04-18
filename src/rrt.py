2a42d3509d83ecb8eea921685bfbf299d0ee01a4
Joseph Camacho <camacho.joseph@gmail.com>
2023-04-18 13:48:34 -0400

    Added a rapidly exploring random tree (connect) in src/rrt.py

---
+++ src/rrt.py
@@ -0,0 +1,156 @@
+# Import required libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define class for RRT node
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.parent = None
+
+class RRT_Connect:
+    def __init__(self, start, goal, obstacle_list):
+        # Initialize tree for start and goal
+        self.start_node = Node(start[0], start[1])
+        self.goal_node = Node(goal[0], goal[1])
+        self.start_tree = [self.start_node]
+        self.goal_tree = [self.goal_node]
+        self.x_range = (min(start[0], goal[0]), max(start[0], goal[0]))
+        self.y_range = (min(start[1], goal[1]), max(start[1], goal[1]))
+        self.obstacle_list = obstacle_list
+
+        self.path = None
+
+
+    def is_collision_free(self, x, y):
+        return all([(x - obstacle[0]) ** 2 + (y - obstacle[1]) ** 2 > obstacle[2] ** 2
+                    for obstacle in self.obstacle_list])
+
+    def get_random_point(self):
+        rand_x = np.random.uniform(*self.x_range)
+        rand_y = np.random.uniform(*self.y_range)
+
+        while not self.is_collision_free(rand_x, rand_y):
+            rand_x = np.random.uniform(*self.x_range)
+            rand_y = np.random.uniform(*self.y_range)
+
+        return rand_x, rand_y
+
+    def extend_start_tree(self, delta=0.1):
+        """
+        Try to extend the start tree. Could be unsuccessful if the new node collides with an object.
+        """
+        rand_x, rand_y = self.get_random_point()
+
+        # Find nearest node in start tree
+        nearest_start_node = min(self.start_tree, key=lambda node: (rand_x - node.x) ** 2 + (rand_y - node.y) ** 2)
+        dist = ((rand_x - nearest_start_node.x) ** 2 + (rand_y - nearest_start_node.y) ** 2) ** 0.5
+
+        # Extend start tree towards random point
+        new_start_node = Node(nearest_start_node.x + delta*(rand_x - nearest_start_node.x)/dist,
+                              nearest_start_node.y + delta*(rand_y - nearest_start_node.y)/dist)
+        if not self.is_collision_free(new_start_node.x, new_start_node.y):
+            return
+        
+        new_start_node.parent = nearest_start_node
+        self.start_tree.append(new_start_node)
+
+        # Check if new node in start tree is close to any node in goal tree
+        for node in self.goal_tree:
+            dist = np.sqrt((new_start_node.x - node.x)**2 + (new_start_node.y - node.y)**2)
+            if dist < delta:
+                # Connect trees and return path
+                path = []
+                node1 = new_start_node
+                node2 = node
+                while node1 is not None:
+                    path.append([node1.x, node1.y])
+                    node1 = node1.parent
+                path.reverse()
+                while node2 is not None:
+                    path.append([node2.x, node2.y])
+                    node2 = node2.parent
+                self.path = path
+                return
+                              
+    def extend_goal_tree(self, delta=0.1):
+        """
+        Try to extend the goal tree. Could be unsuccessful if the new node collides with an object.
+        """      
+        rand_x, rand_y = self.get_random_point()
+        # Find nearest node in goal tree
+        nearest_goal_node = min(self.goal_tree, key=lambda node: (rand_x - node.x) ** 2 + (rand_y - node.y) ** 2)
+        dist = ((rand_x - nearest_goal_node.x) ** 2 + (rand_y - nearest_goal_node.y) ** 2) ** 0.5
+
+        # Extend start tree towards random point
+        new_goal_node = Node(nearest_goal_node.x + delta*(rand_x - nearest_goal_node.x)/dist,
+                              nearest_goal_node.y + delta*(rand_y - nearest_goal_node.y)/dist)
+        
+        if not self.is_collision_free(new_goal_node.x, new_goal_node.y):
+            return
+        
+        new_goal_node.parent = nearest_goal_node
+        self.goal_tree.append(new_goal_node)
+    
+        # Check if new node in goal tree is close to any node in start tree
+        for node in self.start_tree:
+            dist = np.sqrt((new_goal_node.x - node.x)**2 + (new_goal_node.y - node.y)**2)
+            if dist < delta:
+                # Connect trees and return path
+                path = []
+                node1 = node
+                node2 = new_goal_node
+                while node1 is not None:
+                    path.append([node1.x, node1.y])
+                    node1 = node1.parent
+                path.reverse()
+                while node2 is not None:
+                    path.append([node2.x, node2.y])
+                    node2 = node2.parent
+                self.path = path
+                return
+            
+    def get_path(self, max_iter=10000, delta=0.1):
+        for i in range(max_iter):
+            if self.path is not None:
+                return self.path
+            if i%2 == 0:
+                self.extend_start_tree(delta)
+            else:
+                self.extend_goal_tree(delta)
+        return self.path
+
+
+# Define obstacle coordinates
+obstacle_list = [(3, 3, 1), (3, 5, 0.5), (8, 3, 1), (6, 6, 2)]
+
+# Define start and goal coordinates
+start = (0, 0)
+goal = (9, 9)
+
+# Call RRT Connect function
+rrt = RRT_Connect(start, goal, obstacle_list)
+path = rrt.get_path()
+
+# Plot trees
+for node in rrt.start_tree:
+    if node.parent is not None:
+        plt.plot((node.x, node.parent.x), (node.y, node.parent.y), 'b')
+
+for node in rrt.goal_tree:
+    if node.parent is not None:
+        plt.plot((node.x, node.parent.x), (node.y, node.parent.y), 'b')
+
+# Plot path and obstacles
+if path:
+    path = np.array(path)
+    plt.plot(path[:,0], path[:,1], 'r')
+    plt.plot(start[0], start[1], 'bo')
+    plt.plot(goal[0], goal[1], 'yo')
+for obstacle in obstacle_list:
+    circle = plt.Circle(obstacle[:2], obstacle[2], color='k')
+    plt.gcf().gca().add_artist(circle)
+    plt.xlim(0, 10)
+    plt.ylim(0, 10)
+plt.show()
