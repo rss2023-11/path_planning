46032b276790d4477601d214b1c7bbe3e414ca50
Nicole Shigiltchoff <46400495+nicoleshigiltchoff@users.noreply.github.com>
2023-04-19 16:39:07 -0400

    should work better--listens to a publisher now

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -13,21 +13,6 @@ from sklearn.linear_model import LinearRegression
 
 from geometry_msgs.msg import PoseArray, PoseStamped
 from nav_msgs.msg import Odometry
-#!/usr/bin/env python
-
-import rospy
-import numpy as np
-from geometry_msgs.msg import PoseStamped, Twist, Point
-from ackermann_msgs.msg import AckermannDriveStamped
-import math
-import time
-import utils
-import tf
-from numpy.linalg import norm
-from sklearn.linear_model import LinearRegression
-
-from geometry_msgs.msg import PoseArray, PoseStamped
-from nav_msgs.msg import Odometry
 
 class PurePursuit:
     def __init__(self):
@@ -45,11 +30,10 @@ class PurePursuit:
         self.max_angular_speed = 0.34  # Maximum angular speed (rad/s)
         self.wheelbase_length = 0.35  # Distance between front and rear axles of car
         self.trajectory  = utils.LineTrajectory("/followed_trajectory")
-        # self.trajectory = traj_points #remove this line when listening for a trajectory
         self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
         self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
         
-        self.pose_sub = rospy.Subscriber("/pf/pose/odom", Odometry, queue_size = 1)
+        self.pose_sub = rospy.Subscriber("/pf/pose/odom", Odometry, self.define_robot_pose_callback, queue_size = 1) #pose subscriber--tells you where the robot is
 
         # Define variables to store current position and orientation
         self.current_position = None
@@ -58,9 +42,13 @@ class PurePursuit:
         # Define variable to store the target point
         self.target_point = None
 
+    def define_robot_pose_callback(self, msg):
+        self.current_position = msg.pose.pose.position
+        self.orientation = msg.pose.pose.orientation #this is given in quaternians
+
     def find_target_point(self, trajectory):
         # Find the nearest waypoint to the current position as the target point
-        # This will be modified with the circle formula, but for now this is fine
+        # This will be modified with the circle formula, but for now this is fine I think
         nearest_distance = float('inf')
         nearest_point = None
 
@@ -86,66 +74,47 @@ class PurePursuit:
     # def trajectory_callback(self, msg):
     def trajectory_callback(self, msg): #Fxn will be changed to take in a path message once we have that
         # Define control rate. This line will get deleted when listening to a subscriber
-        rate = rospy.Rate(50)
-        rospy.loginfo("trajectory_callback is runnig")
+        rospy.logwarn("trajectory_callback is running")
         
         ''' Clears the currently followed trajectory, and loads the new one from the message
         '''
-        # print "Receiving new trajectory:", len(msg.poses), "points"
-        # self.trajectory.clear()
-        # self.trajectory.fromPoseArray(msg)
+        print "Receiving new trajectory:", len(msg.poses), "points"
+        self.trajectory.clear()
+        self.trajectory.fromPoseArray(msg)
 
-        while not rospy.is_shutdown():
-            # Calculate the distance to the target point
-            self.current_position = msg.pose.position
-            self.current_orientation = msg.pose.orientation
-            
-            rospy.loginfo("calculating distance to target. current pos then target pos") #print statement
-            rospy.loginfo(self.current_position) #print statement
-            rospy.loginfo(self.target_point) #print statement
-            distance = math.sqrt((self.current_position.x - self.target_point.x)**2 + (self.current_position.y - self.target_point.y)**2)
+        # Calculate the distance to the target point
+        self.current_position = msg.pose.position
+        self.current_orientation = msg.pose.orientation
+        
+        rospy.logwarn("calculating distance to target. current pos then target pos") #print statement
+        rospy.logwarn(self.current_position) #print statement
+        rospy.logwarn(self.target_point) #print statement
+        distance = math.sqrt((self.current_position.x - self.target_point.x)**2 + (self.current_position.y - self.target_point.y)**2)
 
-            # If the distance is less than the target distance, find a new target point
-            if distance < self.lookahead:
-                self.target_point = self.find_target_point(waypoints)
+        # If the distance is less than the target distance, find a new target point
+        if distance < self.lookahead:
+            self.target_point = self.find_target_point(waypoints)
 
-            # Calculate the steering angle to the target point
-            steering_angle = self.calculate_steering_angle(self.target_point)
+        # Calculate the steering angle to the target point
+        steering_angle = self.calculate_steering_angle(self.target_point)
+        steering_angle = 0
 
-            # Calculate the linear and angular velocities based on the steering angle and maximum speeds
-            linear_velocity = self.linear_speed
-            angular_velocity = steering_angle * self.max_angular_speed
+        # Calculate the linear and angular velocities based on the steering angle and maximum speeds
+        linear_velocity = self.linear_speed
+        angular_velocity = steering_angle * self.max_angular_speed
 
-            # Create an AckermannDriveStamped message with the linear and angular velocities
-            ack_stamped = AckermannDriveStamped()
-            ack_stamped.drive.speed = linear_velocity
-            ack_stamped.drive.steering_angle = steering_angle
+        # Create an AckermannDriveStamped message with the linear and angular velocities
+        ack_stamped = AckermannDriveStamped()
+        ack_stamped.drive.speed = linear_velocity
+        ack_stamped.drive.steering_angle = steering_angle
 
-            # Publish the AckermannDriveStamped message
-            rospy.loginfo("publishing to driver") #print statement
-            self.drive_pub.publish(ack_stamped)
+        # Publish the AckermannDriveStamped message
+        rospy.logwarn("publishing to driver") #print statement
+        self.drive_pub.publish(ack_stamped)
 
-            # Sleep for the remaining time to maintain the control loop rate
-            rate.sleep()
 
 if __name__=="__main__":
-    rospy.init_node("pure_pursuit")
-    
-    traj_points = [
-    Point(1.0, 0.0, 0.0),
-    Point(2.0, 0.0, 0.0),
-    Point(3.0, 1.0, 0.0),
-    Point(3.0, 2.0, 0.0),
-    Point(2.0, 3.0, 0.0),
-    Point(1.0, 3.0, 0.0),
-    Point(0.0, 2.0, 0.0),
-    Point(0.0, 1.0, 0.0),
-    Point(1.0, 0.0, 0.0)
-    ]
-    
-    message=PoseArray
-    message
-    
+    rospy.init_node("pure_pursuit")    
     pf = PurePursuit()
     # pf.trajectory_callback()
     rospy.spin()
