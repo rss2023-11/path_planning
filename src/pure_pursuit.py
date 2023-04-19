1f2e6789b917a92ed771e650c686fc80e6dbbfd0
Nicole Shigiltchoff <46400495+nicoleshigiltchoff@users.noreply.github.com>
2023-04-19 00:34:51 -0400

    scrapped and restarted a new version of pure pursuit. no errors but nothing happens when run in simulation

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -2,6 +2,9 @@
 
 import rospy
 import numpy as np
+from geometry_msgs.msg import PoseStamped, Twist, Point
+from ackermann_msgs.msg import AckermannDriveStamped
+import math
 import time
 import utils
 import tf
@@ -9,153 +12,140 @@ from numpy.linalg import norm
 from sklearn.linear_model import LinearRegression
 
 from geometry_msgs.msg import PoseArray, PoseStamped
-from visualization_msgs.msg import Marker
+from nav_msgs.msg import Odometry
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+from geometry_msgs.msg import PoseStamped, Twist, Point
 from ackermann_msgs.msg import AckermannDriveStamped
+import math
+import time
+import utils
+import tf
+from numpy.linalg import norm
+from sklearn.linear_model import LinearRegression
+
+from geometry_msgs.msg import PoseArray, PoseStamped
 from nav_msgs.msg import Odometry
 
-class PurePursuit(object):
-    """ Implements Pure Pursuit trajectory tracking with a fixed lookahead and speed.
-    """
+class PurePursuit:
     def __init__(self):
-        self.odom_topic       = rospy.get_param("~odom_topic")
-        self.lookahead        = 1 #should be modified, this is a guess
-        self.speed            = 1 #to be modified probably
-        self.wheelbase_length = 0.35 # this is for ackermann steering
+
+        # Define subscriber to get current pose
+        # rospy.Subscriber('/pose', PoseStamped, self.pose_callback)
+
+        # Define publisher to send drive commands
+        self.drive_pub = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=10)
+
+        # Define control parameters
+        self.odom_topic = rospy.get_param("~odom_topic")
+        self.lookahead = 0.5 #This is a guess  
+        self.linear_speed = 1  
+        self.max_angular_speed = 0.34  # Maximum angular speed (rad/s)
+        self.wheelbase_length = 0.35  # Distance between front and rear axles of car
         self.trajectory  = utils.LineTrajectory("/followed_trajectory")
+        # self.trajectory = traj_points #remove this line when listening for a trajectory
         self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
         self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
-        self.line_pub = rospy.Publisher("/wall", Marker, queue_size=1)
         
         self.pose_sub = rospy.Subscriber("/pf/pose/odom", Odometry, queue_size = 1)
 
-        
-        #should get rid of this when doing pure pursuit?
-        self.K_P = 0.25
-        self.K_I = 0
-        self.K_D = 0.5
-        self.error_I = 0
-        self.prev_error = 0
+        # Define variables to store current position and orientation
+        self.current_position = None
+        self.current_orientation = None
 
-    def trajectory_callback(self, msg):
+        # Define variable to store the target point
+        self.target_point = None
+
+    def find_target_point(self, trajectory):
+        # Find the nearest waypoint to the current position as the target point
+        # This will be modified with the circle formula, but for now this is fine
+        nearest_distance = float('inf')
+        nearest_point = None
+
+        for waypoint in trajectory:
+            distance = math.sqrt((waypoint.x - self.current_position.x)**2 + (waypoint.y - self.current_position.y)**2)
+            if distance < nearest_distance:
+                nearest_distance = distance
+                nearest_point = waypoint
+
+        return nearest_point
+
+    def calculate_steering_angle(self, target_point):
+        # Calculate the steering angle to the target point using Ackermann steering geometry
+        dx = target_point.x - self.current_position.x
+        dy = target_point.y - self.current_position.y
+        alpha = math.atan2(dy, dx)
+        distance = math.sqrt(dx**2 + dy**2)
+
+        steering_angle = math.atan2(2.0 * self.wheelbase_length * math.sin(alpha), distance)
+
+        return steering_angle
+
+    # def trajectory_callback(self, msg):
+    def trajectory_callback(self, msg): #Fxn will be changed to take in a path message once we have that
+        # Define control rate. This line will get deleted when listening to a subscriber
+        rate = rospy.Rate(50)
+        rospy.loginfo("trajectory_callback is runnig")
+        
         ''' Clears the currently followed trajectory, and loads the new one from the message
         '''
-        print "Receiving new trajectory:", len(msg.poses), "points"
-        self.trajectory.clear()
-        self.trajectory.fromPoseArray(msg)
-        
-        #dummy trajectory
-        self.trajectory=[(0,0), (10,0)]
-        
-        #visualize the dummy trajectory
-        VisualizationTools.plot_line(np.array([0,10]), np.array([0,0]), self.line_pub, frame="/laser")
-        
-        self.pose_x = msg.pose.position.x
-        self.pose_y = msg.pose.position.y
-        self.orientation = msg.pose.orientation #this is given in quaternians
-        
-        self.driving_decision()
-        
-        self.trajectory.publish_viz(duration=0.0)
-    
-    def driving_decision(self):
-        ack_stamped = AckermannDriveStamped()
-        ack_stamped.header = Header(stamp=rospy.Time.now())
-        ack_stamped.drive.speed=self.speed
-        
-        #error is distance away from desired path
-        error=self.calc_perp_dist(self.trajectory.points[0][0], self.trajectory.points[0][1], self.trajectory.points[-1][0], self.trajectory.points[-1][1])
-        
-        #will be replaced by pure pursuit later
-        error_P = self.K_P * error
-        self.error_I += self.K_I * error
-        error_D = self.K_D * slope
-        ack_stamped.drive.steering_angle = error_P + self.error_I + error_D
+        # print "Receiving new trajectory:", len(msg.poses), "points"
+        # self.trajectory.clear()
+        # self.trajectory.fromPoseArray(msg)
 
-        # store current error for next D term calculation. we never use this?
-        self.prev_error = error
-        
-        return ack_stamped
-    
-        #remove this and have it publish in trajectory callback when receiving a trajectory
-        self.drive_pub.publish(ack_stamped)s
-        
-    def circle_path_intersect(self):
-        Q = np.array([self.pose_x, self.pose_y]) # Centre of circle
-        r = self.lookahead # Radius of circle
-        P1 = np.array([self.trajectory.points[0][0], self.trajectory.points[0][1]]) # Start of line segment
-        V = np.array([self.trajectory.points[-1][0], self.trajectory.points[-1][1] - P1]) # Vector along line segment
-        #t = 0 #t is a parameter in finding the point P1 + t * V, our desired point
-        
-        a = V.dot(V)
-        b = 2 * V.dot(P1 - Q)
-        c = P1.dot(P1) + Q.dot(Q) - 2 * P1.dot(Q) - r**2
-        
-        disc = b**2 - 4 * a * c
-        if disc < 0:
-            #steer towards the start point (or the end point if it's closer?)
-            pose_start = calc_distance(self.trajectory.points[0][0], self.trajectory.points[0][1], self.pose_x, self.pose_y)
-            pose_end = calc_distance(self.trajectory.points[-1][0], self.trajectory.points[-1][1], self.pose_x, self.pose_y)
-            start_end = calc_distance(self.trajectory.points[0][0], self.trajectory.points[0][1], self.trajectory.points[-1][0], self.trajectory.points[-1][1])
+        while not rospy.is_shutdown():
+            # Calculate the distance to the target point
+            self.current_position = msg.pose.position
+            self.current_orientation = msg.pose.orientation
             
-            if pose_end>start_end and pose_start<pose_end:
-                #return start
-                return np.array([self.trajectory.points[0][0], self.trajectory.points[0][1]])
-            else:
-                #return end
-                return self.trajectory.points[-1][0], self.trajectory.points[-1][1]
-                        
-        elif disc==0: #one root=one intersection
-            t = (-b + sqrt_disc) / (2 * a)
-            if not (0 <= t <= 1):
-                if t<0:
-                    #return start
-                    return np.array([self.trajectory.points[0][0], self.trajectory.points[0][1]])
-                elif t<1:
-                    #return end
-                    return self.trajectory.points[-1][0], self.trajectory.points[-1][1]
-            
-        else: #2 intersections
-            t1 = (-b + sqrt_disc) / (2 * a)
-            t2 = (-b - sqrt_disc) / (2 * a)
+            rospy.loginfo("calculating distance to target. current pos then target pos") #print statement
+            rospy.loginfo(self.current_position) #print statement
+            rospy.loginfo(self.target_point) #print statement
+            distance = math.sqrt((self.current_position.x - self.target_point.x)**2 + (self.current_position.y - self.target_point.y)**2)
 
-            if (0 <= t1 <= 1 and 0 <= t2 <= 1): #both on line segment
-                t1_end = calc_distance(t1[0], t1[1], self.trajectory.points[-1][0], self.trajectory.points[-1][1])
-                t2_end = calc_distance(t2[0], t2[1], self.trajectory.points[-1][0], self.trajectory.points[-1][1])
-                
-                if t2_end>=t1_end:
-                    t=t2
-                else:
-                    t=t1
-            
-            if not (0 <= t1 and t1 <= 1) or (0 <= t2 and t2 <= 1): #neither on line segment--segment is inside the circle
-                #return end
-                return self.trajectory.points[-1][0], self.trajectory.points[-1][1]
-                
-            elif 0 <= t2 and t2 <= 1: #t1 only on line segment
-                t=t2
-                
-            elif 0 <= t2 and t2 <= 1: #t2 only on line segment
-                t=t1
-                
-            return P1 + t * V
-        
-    def calc_perp_dist(self, x1, y1, x2, y2):
-        
-        pose=np.array([self.pose_x, self.pose_y])
-        line_start=np.array([x1, y1])
-        line_end=np.array([x2, y2])
-        
-        return np.cross(line_end-line_start,pose-line_start)/norm(line_end-line_start)
+            # If the distance is less than the target distance, find a new target point
+            if distance < self.lookahead:
+                self.target_point = self.find_target_point(waypoints)
 
-    def calc_distance(x1, y1, x2, y2):
-    #calculates distance between (x1, y1) and (x2, y2)
-        p1=np.array([x1, y1])
-        p2=np.array([x2, y2])
-        
-        return norm(p1-p2)
+            # Calculate the steering angle to the target point
+            steering_angle = self.calculate_steering_angle(self.target_point)
+
+            # Calculate the linear and angular velocities based on the steering angle and maximum speeds
+            linear_velocity = self.linear_speed
+            angular_velocity = steering_angle * self.max_angular_speed
+
+            # Create an AckermannDriveStamped message with the linear and angular velocities
+            ack_stamped = AckermannDriveStamped()
+            ack_stamped.drive.speed = linear_velocity
+            ack_stamped.drive.steering_angle = steering_angle
+
+            # Publish the AckermannDriveStamped message
+            rospy.loginfo("publishing to driver") #print statement
+            self.drive_pub.publish(ack_stamped)
+
+            # Sleep for the remaining time to maintain the control loop rate
+            rate.sleep()
 
 if __name__=="__main__":
     rospy.init_node("pure_pursuit")
+    
+    traj_points = [
+    Point(1.0, 0.0, 0.0),
+    Point(2.0, 0.0, 0.0),
+    Point(3.0, 1.0, 0.0),
+    Point(3.0, 2.0, 0.0),
+    Point(2.0, 3.0, 0.0),
+    Point(1.0, 3.0, 0.0),
+    Point(0.0, 2.0, 0.0),
+    Point(0.0, 1.0, 0.0),
+    Point(1.0, 0.0, 0.0)
+    ]
+    
+    message=PoseArray
+    message
+    
     pf = PurePursuit()
-    pf.driving_decision() #remove this line when implementing all this with a trajectory
+    # pf.trajectory_callback()
     rospy.spin()
---
+++ src/pure_pursuit_old.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import time
+import utils
+import tf
+from numpy.linalg import norm
+from sklearn.linear_model import LinearRegression
+
+from geometry_msgs.msg import PoseArray, PoseStamped
+from visualization_msgs.msg import Marker
+from ackermann_msgs.msg import AckermannDriveStamped
+from nav_msgs.msg import Odometry
+
+class PurePursuit(object):
+    """ Implements Pure Pursuit trajectory tracking with a fixed lookahead and speed.
+    """
+    def __init__(self):
+        self.odom_topic       = rospy.get_param("~odom_topic")
+        self.lookahead        = 1 #should be modified, this is a guess
+        self.speed            = 1 #to be modified probably
+        self.wheelbase_length = 0.35 # this is for ackermann steering
+        self.trajectory  = utils.LineTrajectory("/followed_trajectory")
+        self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
+        self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
+        self.line_pub = rospy.Publisher("/wall", Marker, queue_size=1)
+        
+        self.pose_sub = rospy.Subscriber("/pf/pose/odom", Odometry, queue_size = 1)
+
+        
+        #should get rid of this when doing pure pursuit?
+        self.K_P = 0.25
+        self.K_I = 0
+        self.K_D = 0.5
+        self.error_I = 0
+        self.prev_error = 0
+
+    def trajectory_callback(self, msg):
+        ''' Clears the currently followed trajectory, and loads the new one from the message
+        '''
+        print "Receiving new trajectory:", len(msg.poses), "points"
+        self.trajectory.clear()
+        self.trajectory.fromPoseArray(msg)
+        
+        #dummy trajectory
+        self.trajectory=[(0,0), (10,0)]
+        
+        #visualize the dummy trajectory
+        VisualizationTools.plot_line(np.array([0,10]), np.array([0,0]), self.line_pub, frame="/laser")
+        
+        self.pose_x = msg.pose.position.x
+        self.pose_y = msg.pose.position.y
+        self.orientation = msg.pose.orientation #this is given in quaternians
+        
+        self.driving_decision()
+        
+        self.trajectory.publish_viz(duration=0.0)
+    
+    def driving_decision(self):
+        ack_stamped = AckermannDriveStamped()
+        ack_stamped.header = Header(stamp=rospy.Time.now())
+        ack_stamped.drive.speed=self.speed
+        
+        #error is distance away from desired path
+        error=self.calc_perp_dist(self.trajectory.points[0][0], self.trajectory.points[0][1], self.trajectory.points[-1][0], self.trajectory.points[-1][1])
+        
+        #will be replaced by pure pursuit later
+        error_P = self.K_P * error
+        self.error_I += self.K_I * error
+        error_D = self.K_D * slope
+        ack_stamped.drive.steering_angle = error_P + self.error_I + error_D
+
+        # store current error for next D term calculation. we never use this?
+        self.prev_error = error
+        
+        return ack_stamped
+    
+        #remove this and have it publish in trajectory callback when receiving a trajectory
+        self.drive_pub.publish(ack_stamped)s
+        
+    def circle_path_intersect(self):
+        Q = np.array([self.pose_x, self.pose_y]) # Centre of circle
+        r = self.lookahead # Radius of circle
+        P1 = np.array([self.trajectory.points[0][0], self.trajectory.points[0][1]]) # Start of line segment
+        V = np.array([self.trajectory.points[-1][0], self.trajectory.points[-1][1] - P1]) # Vector along line segment
+        #t = 0 #t is a parameter in finding the point P1 + t * V, our desired point
+        
+        a = V.dot(V)
+        b = 2 * V.dot(P1 - Q)
+        c = P1.dot(P1) + Q.dot(Q) - 2 * P1.dot(Q) - r**2
+        
+        disc = b**2 - 4 * a * c
+        if disc < 0:
+            #steer towards the start point (or the end point if it's closer?)
+            pose_start = calc_distance(self.trajectory.points[0][0], self.trajectory.points[0][1], self.pose_x, self.pose_y)
+            pose_end = calc_distance(self.trajectory.points[-1][0], self.trajectory.points[-1][1], self.pose_x, self.pose_y)
+            start_end = calc_distance(self.trajectory.points[0][0], self.trajectory.points[0][1], self.trajectory.points[-1][0], self.trajectory.points[-1][1])
+            
+            if pose_end>start_end and pose_start<pose_end:
+                #return start
+                return np.array([self.trajectory.points[0][0], self.trajectory.points[0][1]])
+            else:
+                #return end
+                return self.trajectory.points[-1][0], self.trajectory.points[-1][1]
+                        
+        elif disc==0: #one root=one intersection
+            t = (-b + sqrt_disc) / (2 * a)
+            if not (0 <= t <= 1):
+                if t<0:
+                    #return start
+                    return np.array([self.trajectory.points[0][0], self.trajectory.points[0][1]])
+                elif t<1:
+                    #return end
+                    return self.trajectory.points[-1][0], self.trajectory.points[-1][1]
+            
+        else: #2 intersections
+            t1 = (-b + sqrt_disc) / (2 * a)
+            t2 = (-b - sqrt_disc) / (2 * a)
+
+            if (0 <= t1 <= 1 and 0 <= t2 <= 1): #both on line segment
+                t1_end = calc_distance(t1[0], t1[1], self.trajectory.points[-1][0], self.trajectory.points[-1][1])
+                t2_end = calc_distance(t2[0], t2[1], self.trajectory.points[-1][0], self.trajectory.points[-1][1])
+                
+                if t2_end>=t1_end:
+                    t=t2
+                else:
+                    t=t1
+            
+            if not (0 <= t1 and t1 <= 1) or (0 <= t2 and t2 <= 1): #neither on line segment--segment is inside the circle
+                #return end
+                return self.trajectory.points[-1][0], self.trajectory.points[-1][1]
+                
+            elif 0 <= t2 and t2 <= 1: #t1 only on line segment
+                t=t2
+                
+            elif 0 <= t2 and t2 <= 1: #t2 only on line segment
+                t=t1
+                
+            return P1 + t * V #np array [x, y] of goal point
+        
+        
+        
+    def calc_perp_dist(self, x1, y1, x2, y2):
+        
+        pose=np.array([self.pose_x, self.pose_y])
+        line_start=np.array([x1, y1])
+        line_end=np.array([x2, y2])
+        
+        return np.cross(line_end-line_start,pose-line_start)/norm(line_end-line_start)
+
+    def calc_distance(x1, y1, x2, y2):
+    #calculates distance between (x1, y1) and (x2, y2)
+        p1=np.array([x1, y1])
+        p2=np.array([x2, y2])
+        
+        return norm(p1-p2)
+
+if __name__=="__main__":
+    rospy.init_node("pure_pursuit")
+    pf = PurePursuit()
+    pf.driving_decision() #remove this line when implementing all this with a trajectory
+    rospy.spin()
